da3d3afcee6e732a6b2df3b1098b3541ee2cd9c0
Justin Hong <jjhong922@berkeley.edu>
2023-02-06 12:47:13 -0500

    final print

--- R/sleepNonInteractiveAddin.R
+++ R/sleepNonInteractiveAddin.R
@@ -1,6 +1,7 @@
-#' sleep for 10 seconds
+#' sleep for 10 seconds then print something to console
 #'
 #' @export
 sleepNonInteractiveAddin <- function() {
     Sys.sleep(10)
+    print("I'm awake.")
 }
